d24f1d9914486f919cb16876176694fc230e88cf
ezulkosk <ed.zulkoski@gmail.com>
2026-08-25 16:55:37 +0000

    buggy examples

---
+++ chapter1/buggy_course_average.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int grade1, grade2, grade3, grade4, grade5;
+    cout << "Enter 5 grades: ";
+    cin >> grade1 >> grade2 >> grade3 >> grade4 >> grade5;
+
+    int average = (grade1 / 5) + (grade2 / 5) + (grade3 / 5) + (grade4 / 5) + (grade5 / 5);
+    
+    cout << average << endl;
+
+    return 0;
+}
---
+++ chapter1/buggy_grades.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // prompt the user for a number grade
+    int grade;
+    cout << "Enter grade: ";
+    cin >> grade;
+
+    // compute the equivalent letter grade
+    if (grade > 90)
+        cout << "A" << endl;
+    else if(grade > 80) 
+        cout << "B" << endl;
+    else if(grade > 70) 
+        cout << "C" << endl;
+    else if(grade > 60) 
+        cout << "D" << endl;
+    else 
+        cout << "E" << endl;
+
+    return 0;
+}
