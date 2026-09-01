49144e73fb351b434aad1640120edb796eb4372a
ezulkosk <ed.zulkoski@gmail.com>
2026-09-01 21:29:07 +0000

    another example

---
+++ chapter2/example_2_14.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1, num2, num3;
+
+    // 1
+    num = 18;
+    
+    // 2
+    num1 = num1 + 27;
+
+    // 3
+    num2 = num1;
+
+    // 4
+    num3 = num2 / 5;
+
+    // 5
+    num3 = num3 / 4;
+
+
+    return 0;
+}
