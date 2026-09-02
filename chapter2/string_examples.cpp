2b3d32ec157b243e409c72f05e7b447595f14436
ezulkosk <ed.zulkoski@gmail.com>
2026-09-02 13:51:30 +0000

    string examples

---
+++ chapter2/string_examples.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s = "Hello, World!";
+    cout << s << endl;
+    
+    cout << "---" << endl;
+    cout << "String\twith\ttabs" << endl;
+    
+    cout << "---" << endl;
+    cout<< "String\nwith\nnewlines\n" << endl;
+    
+    cout << "---" << endl;
+    cout<< "\"Quoted String\"" << endl;
+    
+    cout << "---" << endl;
+    cout << s + s << endl;
+
+    return 0;
+}
